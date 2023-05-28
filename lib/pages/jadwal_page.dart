d1cf63bf197b637a910f3ddeb229daf3d9c8d9a3
shintanr <shintanurrohmah143@gmail.com>
2023-05-28 08:38:54 +0700

    add jadwal page

---
+++ lib/pages/jadwal_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class JadwalPage extends StatefulWidget {
+  const JadwalPage({super.key});
+
+  @override
+  State<JadwalPage> createState() => _JadwalPageState();
+}
+
+class _JadwalPageState extends State<JadwalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: ListView(),
+      ),
+    );
+  }
+}
